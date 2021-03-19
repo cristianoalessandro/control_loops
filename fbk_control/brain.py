00f3a6d28cc94455da60f1d1879d37d84a0845d9
Cristiano Alessandro <cri.alessandro@gmail.com>
2021-03-19 11:09:49 +0100

    structure

---
+++ fbk_control/brain.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+
+import nest
+import numpy as np
+import time
+import sys
+import music
+import matplotlib.pyplot as plt
+
+# Just to get the following imports right!
+sys.path.insert(1, '../')
+
+import trajectories as tj
+from motorcortex import MotorCortex
+from population_view import plotPopulation
+from util import savePattern
+from population_view import PopView
+from pointMass import PointMass
+from settings import Experiment, Simulation, Brain
+
+nest.Install("util_neurons_module")
+
+
+########### Simulation
+
+sim = Simulation()
+
+res       = sim.resolution
+time_span = sim.timeMax
+time_vect = np.linspace(0, time_span, num=int(np.round(time_span/res)), endpoint=True)
+
+nest.SetKernelStatus({"resolution": res})
+nest.SetKernelStatus({"overwrite_files": True})
+
+# Randomize
+msd = int( time.time() * 1000.0 )
+N_vp = nest.GetKernelStatus(['total_num_virtual_procs'])[0]
+nest.SetKernelStatus({'rng_seeds' : range(msd+N_vp+1, msd+2*N_vp+1)})
+
+
+##################### Experiment
+
+exp = Experiment()
+
+dynSys   = exp.dynSys
+njt      = exp.dynSys.numVariables()
+
+init_pos = exp.IC_pos
+tgt_pos  = exp.tgt_pos
+trj, pol = tj.minimumJerk(init_pos, tgt_pos, time_vect)
+
+pthDat   = exp.pathData
+
+
+####### Motor cortex
+
+brain = Brain()
+
+# Number of neurons (for each subpopulation positive/negative)
+N = brain.nNeurPop
+
+# Precise or approximatesd motor control?
+preciseControl = brain.precCtrl
+
+# Motor cortex parameters
+mc_param = brain._motCtx_param
+
+# Create motor cortex
+mc = MotorCortex(N, time_vect, trj, dynSys, pthDat, preciseControl, **mc_param)
+
+
+######## Create MUSIC output
+
+proxy_out = nest.Create('music_event_out_proxy', 1, params = {'port_name':'music_out'})
+
+ii=0
+for j in range(njt):
+    for i, n in enumerate(mc.out_p[j].pop):
+        nest.Connect([n], proxy_out, "one_to_one",{'music_channel': ii})
+        ii=ii+1
+    for i, n in enumerate(mc.out_n[j].pop):
+        nest.Connect([n], proxy_out, "one_to_one",{'music_channel': ii})
+        ii=ii+1
+
+
+###################### SIMULATE
+# Simulate
+nest.Simulate(time_span)
+
+
+############# PLOTTING
+
+
+# motCmd = mc.getMotorCommands()
+# fig, ax = plt.subplots(2,1)
+# ax[0].plot(time_vect,trj)
+# ax[1].plot(time_vect,motCmd)
+#
+#
+# lgd = ['x','y']
+#
+# fig, ax = plt.subplots(2,1)
+# for i in range(njt):
+#     mc.out_p[i].plot_rate(time_vect,ax=ax[i],bar=False,color='r',label='out')
+#     mc.out_n[i].plot_rate(time_vect,ax=ax[i],bar=False,title=lgd[i]+" (Hz)",color='b')
+#
+#     b,c,pos_r = mc.out_p[i].computePSTH(time_vect,buffer_sz=25)
+#     b,c,neg_r = mc.out_n[i].computePSTH(time_vect,buffer_sz=25)
+#     if i==0:
+#         plt.figure()
+#     plt.plot(b[:-1],pos_r-neg_r)
+#     plt.xlabel("time (ms)")
+#     plt.ylabel("spike rate positive - negative")
+#     plt.legend(lgd)
+#
+# #plt.savefig("mctx_out_pos-neg.png")
+# plt.show()
---
+++ fbk_control/settings.py
@@ -0,0 +1,156 @@
+"""Experiment"""
+
+__authors__ = "Cristiano Alessandro"
+__copyright__ = "Copyright 2021"
+__credits__ = ["Cristiano Alessandro"]
+__license__ = "GPL"
+__version__ = "1.0.1"
+
+
+import sys
+import numpy as np
+
+
+# Just to get the following imports right!
+sys.path.insert(1, '../')
+
+from pointMass import PointMass
+import perturbation as pt
+
+
+####################################################################
+class Experiment:
+
+    def __init__(self):
+
+        # Where to save data
+        self._pathData = "./data/"
+
+        # Initial conditions
+        self._IC_pos = np.array([0.0,0.0])
+        self._IC_vel = np.array([0.0,0.0])
+
+        # Target position
+        self._tgt_pos  = np.array([0.0,0.5])
+
+        # Perturbation
+        self._frcFld_angle = 0
+        self._frcFld_k     = 1
+
+        # Dynamical system to be controlled (mass and dyn sys object)
+        self._m      = 2.0
+        self._dynSys = PointMass(mass=self._m, IC_pos=self._IC_pos, IC_vel=self._IC_vel)
+
+    @property
+    def pathData(self):
+        return self._pathData
+
+    @property
+    def dynSys(self):
+        return self._dynSys
+
+    @property
+    def IC_pos(self):
+        return self._IC_pos
+
+    @property
+    def IC_vel(self):
+        return self._IC_vel
+
+    @property
+    def tgt_pos(self):
+        return self._tgt_pos
+
+    @property
+    def frcFld_angle(self):
+        return self._frcFld_angle
+
+    @property
+    def frcFld_k(self):
+        return self._frcFld_k
+
+
+####################################################################
+class Simulation():
+
+    def __init__(self):
+
+        # Nest resolution (milliseconds)
+        self._resolution = 0.1
+
+        # Simulation time (milliseconds)
+        self._timeMax = 1000.0
+
+    @property
+    def resolution(self):
+        return self._resolution
+
+    @property
+    def timeMax(self):
+        return self._timeMax
+
+
+####################################################################
+class Brain():
+
+    def __init__(self):
+
+        # Number of neurons for each subpopulation (positive/negative)
+        self._nNeurPop = 20
+
+        # Initialize motor cortex settings
+        self.initMotorCortex()
+
+        # Initialize spinal cord settings
+        self.initSpine()
+
+
+    def initMotorCortex(self):
+
+        # If true, motor cortex computes precise motor commands using inv. dynamics
+        self._precCtrl = True
+
+        self._motCtx_param = {
+            "ffwd_base_rate":  0.0, # Feedforward neurons
+            "ffwd_kp":        10.0,
+            "fbk_base_rate":   0.0, # Feedback neurons
+            "fbk_kp":         10.0,
+            "out_base_rate":   0.0, # Output neurons
+            "out_kp":          1.0,
+            "wgt_ffwd_out":    1.0, # Connection weight from ffwd to output neurons (must be positive)
+            "wgt_fbk_out":     1.0  # Connection weight from fbk to output neurons (must be positive)
+            }
+
+    def initSpine(self):
+
+        self._spine_param = {
+            "wgt_motCtx_motNeur" : 1.0
+            }
+
+    @property
+    def nNeurPop(self):
+        return self._nNeurPop
+
+    @property
+    def motCtx_param(self):
+        return self._motCtx_param
+
+    @property
+    def precCtrl(self):
+        return self._precCtrl
+
+    @property
+    def spine_param(self):
+        return self._spine_param
+
+
+####################################################################
+class MusicCfg():
+
+    def __init__(self):
+
+        self._input_latency = 0.0001 # seconds
+
+    @property
+    def input_latency(self):
+        return self._input_latency
